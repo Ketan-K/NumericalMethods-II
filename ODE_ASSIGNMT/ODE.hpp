3d2724a9091980917e50fb213479e36c914eafe2
Ketan-K <42907652+Ketan-K@users.noreply.github.com>
2019-11-30 15:53:40 +0530

    Update ODE.hpp

--- ODE_ASSIGNMT/ODE.hpp
+++ ODE_ASSIGNMT/ODE.hpp
@@ -1,9 +1,13 @@
-#include <iostream>
-using namespace std;
 class ODE
 {
-public:
-    ODE();
-    double function(double, double);
-    double eulersMethod(double, double, double, double);
+	public :
+		double improvedTangent(double,double,double,double);
+		double eulersCauchy(double,double,double,double);
+		double Euler(double,double,double,double);
+		double classicalRungeKuttaMethod(double,double,double,double);
+		double Heun(double,double,double,double);
+		double NystormMethod(double,double,double,double);
+		double RungeKutta(double,double,double,double);
+		double classicalMethod(double,double,double,double);
+		double functionY(double,double);	
 };
